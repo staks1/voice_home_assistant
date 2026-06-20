aabab664df8f4ca8d46a81e216d8e2810fd58130
staks1 <sta8srules@gmail.com>
2026-06-20 10:21:30 -0400

    Modify training pipeline to integrate the new dataset train/val splitting logic to create the datasets for training

--- src/training_pipeline.py
+++ src/training_pipeline.py
@@ -1,5 +1,6 @@
 
 from utils.training_logger import TrainingLogger
+from utils.create_datasets import prepare_dataloaders
 from dynamic_dataset_class import DynamicKeywordDataset
 from torch.utils.data import DataLoader
 import os 
@@ -25,7 +26,6 @@ train_logger = TrainingLogger(path_to_logs)
 #data_list = [("path/to/yes_01.wav", 0), ("path/to/no_01.wav", 1), ...]
 data_list = [ (os.path.join(dataset_path,x,file),class_mapping.get(x)) for x in os.listdir(dataset_path) 
                         for file in os.listdir(os.path.join(dataset_path, x))]
-train_dataset = DynamicKeywordDataset(data_list, noise_dir=os.path.join(dataset_path,noise_path), is_training=True)
 
 
 
@@ -33,10 +33,28 @@ train_dataset = DynamicKeywordDataset(data_list, noise_dir=os.path.join(dataset_
 
 if __name__ == "__main__":
     # this potentially needs thinking, what batch size we will use ??
-    train_loader = iter(DataLoader(train_dataset, batch_size=64, shuffle=True, num_workers=4))
+    #prepare_dataloaders(full_data_list, noise_dir, class_mapping, batch_size=64, val_split=0.1, plot_distribution_per_class=False)
+    train_loader, val_loader = prepare_dataloaders(
+    full_data_list=data_list, 
+    noise_dir=os.path.join(dataset_path, noise_path),
+    class_mapping=class_mapping,  # Pass your class_mapping here
+    batch_size=4, 
+    val_split=0.1,
+    plot_distribution_per_class=False,  # Enable plotting
+    pin_memory=False
+)
+    # test train loader 
+    print("==== train ====\n")
+    train_loader = iter(train_loader)
+    waveform, label = next(train_loader)
+    print("tensor", waveform)
+    print("label", label)
+    print("tensor shape", waveform.shape)
 
-
-    #print(next(train_loader))
-    print("tensor",next(train_loader)[0])
-    print("label",next(train_loader[1]))
-    print("tensor shape",next(train_loader)[0].shape)
+    print("==== val ====\n")
+    # test val loader 
+    val_loader = iter(val_loader)
+    waveform, label = next(val_loader)
+    print("tensor", waveform)
+    print("label", label)
+    print("tensor shape", waveform.shape)
